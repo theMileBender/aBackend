53483cf60f2e0cea3c616932463c91fe17932ae6
Harsha <harsha@minebrat.com>
2019-07-16 22:36:39 +0530

    Added router

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
 // Import the express lirbary
 const express = require('express');
 
+// Inititalizing router
+const router = express.Router(); 
+
+// Body parser
+const bodyParser = require('body-parser');
+
 // Create a new express application and use
 // the express static middleware, to serve all files
 // inside the public directory
 const app = express();
-app.use(express.static(__dirname + '/public'))
+app.use(express.static(__dirname + '/public'));
 
 // Start the server on port 8080
 app.listen(8080);
 console.log("App listening on 8080");
+
+app.use("/api/v1/", require('./routes/api/airquality'));
